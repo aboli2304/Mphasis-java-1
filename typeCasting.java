8091af8ae5beb50f8f418ad822756aaa0a286731
github_aboli2304 <abolicholachgudd1223@gmail.com>
2022-02-04 15:47:17 +0530

    typeCasting

---
+++ typeCasting.java
@@ -0,0 +1,33 @@
+public class typeCasting {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Implicit  Casting");
+		char a='A';
+		System.out.println("Value of a: "+a);
+		
+		int b=a;
+		System.out.println("Value of b: "+b);
+		
+		float c=a;
+		System.out.println("Value of c: "+c);
+		
+		long d=a;
+		System.out.println("Value of d: "+d);
+		
+		double e=a;
+		System.out.println("Value of e: "+e);
+		
+				
+		System.out.println("\n");
+		
+		System.out.println("Explicit Casting");
+		
+		
+		double x=22.5;
+		int y=(int)x;
+		System.out.println("Value of x: "+x);
+		System.out.println("Value of y: "+y);
+		
+	}
+}
